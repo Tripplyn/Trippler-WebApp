0be2288e78e2d26e3253713c65c49a8b2d644841
Yuri Winche Achermann <yuri.achermann@gmail.com>
2023-01-05 15:56:32 -0300

    :tada: First commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,19 +19,17 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             IPTeta <span className="text-gray-900">Travel</span> App
           </h1>
-          <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="w-full max-w-screen-xl lg:w-2/3">
-              <div className="border-tdgreen relative overflow-hidden rounded-lg border-2 border-solid shadow drop-shadow-md">
-                <div className="flex items-center justify-center">
-                  <iframe
-                    width="1000"
-                    height="500"
-                    src="https://www.youtube.com/embed/yxq0ZnjEENs"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
-                </div>
+          <div className="w-full max-w-screen-xl sm:w-2/3 md:w-2/3 lg:w-2/3">
+            <div className="border-tdgreen relative overflow-hidden rounded-lg border-2 border-solid shadow drop-shadow-md">
+              <div className="flex items-center justify-center">
+                <iframe
+                  width="1000"
+                  height="500"
+                  src="https://www.youtube.com/embed/yxq0ZnjEENs"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
               </div>
             </div>
           </div>
