e7dacafe4b70c375a7ec133082ee1abbc2c5726e
Yuri Winche Achermann <yuri.achermann@gmail.com>
2023-01-06 11:18:36 -0300

    :art: Changing video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
                 <iframe
                   width="1000"
                   height="500"
-                  src="https://www.youtube.com/embed/yxq0ZnjEENs"
+                  src="https://www.youtube.com/embed/_OV70wVUB3U"
                   title="YouTube video player"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
